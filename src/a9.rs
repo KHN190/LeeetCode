1c8144cb19fcd39908d3992be0896b76a88f1e6f
knh190 <knh190@hotmail.com>
2021-07-14 07:35:25 -0400

    update.

---
+++ src/a9.rs
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/palindrome-number/
+
+// 0. 和 a7 解法一樣
+// 1. 能用 stack 解決嗎
+
+// run test cases
+pub fn main() {
+    assert_eq!(is_palindrome(10), false);
+    assert_eq!(is_palindrome(11), true);
+    assert_eq!(is_palindrome(121), true);
+    assert_eq!(is_palindrome(5), true);
+    assert_eq!(is_palindrome(-121), false);
+    assert_eq!(is_palindrome(313), true);
+    assert_eq!(is_palindrome(1021), false);
+
+    println!("success.");
+}
+
+pub fn is_palindrome(x: i32) -> bool {
+    // special case
+    if x < 0 || (x > 0 && x % 10 == 0) { return false; }
+
+    // get reversed number
+    let rev = crate::a7::reverse(x);
+
+    x == rev
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod a1;
 pub mod a1805;
 pub mod a7;
+pub mod a9;
 
 fn main() {
     a1805::main();
     a1::main();
     a7::main();
+    a9::main();
 }
