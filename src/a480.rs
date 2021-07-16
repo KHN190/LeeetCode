8b05391d950866c362f88a22fd8e27dc5fd588c1
knh190 <knh190@hotmail.com>
2021-07-16 03:32:04 -0400

    fmt.

--- src/a480.rs
+++ src/a480.rs
@@ -40,86 +40,118 @@
 //         bisect.insort(window, b)
 //     return medians
 
+use std::cmp::Reverse; // MaxHeap
+use std::collections::BinaryHeap; // for MinHeap
+
 #[test]
 fn run_test() {
     // test different windows
-    let nums = vec!(1,2,3,4,5,1,2);
-    assert_eq!(median_sliding_window(nums.clone(), 3), vec!(2.0, 3.0, 4.0, 4.0, 2.0));
-    assert_eq!(median_sliding_window(nums.clone(), 4), vec!(2.5, 3.5, 3.5, 3.0));
+    let nums = vec![1, 2, 3, 4, 5, 1, 2];
+    assert_eq!(
+        median_sliding_window(nums.clone(), 3),
+        vec!(2.0, 3.0, 4.0, 4.0, 2.0)
+    );
+    assert_eq!(
+        median_sliding_window(nums.clone(), 4),
+        vec!(2.5, 3.5, 3.5, 3.0)
+    );
 
     // test delete only once
-    let nums = vec!(1,2,3,4,2,3,1,4,2);
-    assert_eq!(median_sliding_window(nums.clone(), 3), vec!(2.0, 3.0, 3.0, 3.0, 2.0, 3.0, 2.0));
+    let nums = vec![1, 2, 3, 4, 2, 3, 1, 4, 2];
+    assert_eq!(
+        median_sliding_window(nums.clone(), 3),
+        vec!(2.0, 3.0, 3.0, 3.0, 2.0, 3.0, 2.0)
+    );
 
     // test overflow
-    let nums = vec!(2147483647, 2147483647);
+    let nums = vec![2147483647, 2147483647];
     assert_eq!(median_sliding_window(nums.clone(), 2), vec!(2147483647.0));
 }
 
 pub fn median_sliding_window(nums: Vec<i32>, k: i32) -> Vec<f64> {
-    use std::collections::BinaryHeap; // MaxHeap
-    use std::cmp::Reverse; // for MinHeap
-
-    // remove an element and rebuild heap
-    fn rebuild_lhs_without(lhs: BinaryHeap<i32>, x: i32) -> BinaryHeap<i32> {
-        let mut heap = BinaryHeap::<i32>::new();
-        let mut removed_once = false;
-        for c in lhs.into_sorted_vec() {
-            if c == x && !removed_once { removed_once = true; }
-            else { heap.push(c); }
-        };
-        heap
-    }
-    fn rebuild_rhs_without(rhs: BinaryHeap<Reverse<i32>>, x: i32) -> BinaryHeap<Reverse<i32>> {
-        let mut heap = BinaryHeap::<Reverse<i32>>::new();
-        let mut removed_once = false;
-        for c in rhs.into_sorted_vec() {
-            if c.0 == x && !removed_once { removed_once = true; }
-            else { heap.push(c); }
-        };
-        heap
-    }
-    // rhs always has 1 more element
-    fn balance(lhs: &mut BinaryHeap<i32>, rhs: &mut BinaryHeap<Reverse<i32>>) {
-        let s1 = lhs.len() as i32;
-        let s2 = rhs.len() as i32;
-        if s1 - s2 >= 1 { rhs.push(Reverse(lhs.pop().unwrap())); }
-        if s2 - s1 > 1 { lhs.push(rhs.pop().unwrap().0); }
-    }
-
     let mut lhs = BinaryHeap::<i32>::new();
     let mut rhs = BinaryHeap::<Reverse<i32>>::new();
 
-    let mut res: Vec<f64> = vec!();
+    let mut res: Vec<f64> = vec![];
     // insert all numbers, when exceeds the lhs limit,
     // move it to rhs.
     for (ix, i) in nums.iter().enumerate() {
         // which side should i go?
-        if rhs.is_empty() || (!rhs.is_empty() && rhs.peek().unwrap().0 < *i) { rhs.push(Reverse(*i)); }
-        else { lhs.push(*i); }
+        if rhs.is_empty() || (!rhs.is_empty() && rhs_top(&rhs) < *i) {
+            rhs.push(Reverse(*i));
+        } else {
+            lhs.push(*i);
+        }
 
         balance(&mut lhs, &mut rhs);
 
         // remove oldest elem
         let total = (rhs.len() + lhs.len()) as i32;
         if total > k {
-            let oldest = &nums[ix - k as usize];
+            let oldest = nums[ix - k as usize];
             // remove oldest
-            if oldest >= &rhs.peek().unwrap().0 { rhs = rebuild_rhs_without(rhs, *oldest); }
-            else { lhs = rebuild_lhs_without(lhs, *oldest); }
+            if oldest >= rhs_top(&rhs) {
+                rhs = rebuild_rhs_without(rhs, oldest);
+            } else {
+                lhs = rebuild_lhs_without(lhs, oldest);
+            }
 
             balance(&mut lhs, &mut rhs);
         }
         // we return the median!
         if total >= k {
             let median = if k % 2 == 1 {
-                rhs.peek().unwrap().0 as f64
+                rhs_top(&rhs) as f64
             } else {
                 // corner case: overflow!
-                (*lhs.peek().unwrap()) as f64 * 0.5f64 + rhs.peek().unwrap().0 as f64 * 0.5f64
+                lhs_top(&lhs) as f64 * 0.5 + rhs_top(&rhs) as f64 * 0.5
             };
             res.push(median);
         }
     }
     res
+}
+
+// remove an element and rebuild heap
+fn rebuild_lhs_without(lhs: BinaryHeap<i32>, x: i32) -> BinaryHeap<i32> {
+    let mut heap = BinaryHeap::<i32>::new();
+    let mut removed_once = false;
+    for c in lhs.into_sorted_vec() {
+        if c == x && !removed_once {
+            removed_once = true;
+        } else {
+            heap.push(c);
+        }
+    }
+    heap
+}
+fn rebuild_rhs_without(rhs: BinaryHeap<Reverse<i32>>, x: i32) -> BinaryHeap<Reverse<i32>> {
+    let mut heap = BinaryHeap::<Reverse<i32>>::new();
+    let mut removed_once = false;
+    for c in rhs.into_sorted_vec() {
+        if c.0 == x && !removed_once {
+            removed_once = true;
+        } else {
+            heap.push(c);
+        }
+    }
+    heap
+}
+// rhs always has 1 more element
+fn balance(lhs: &mut BinaryHeap<i32>, rhs: &mut BinaryHeap<Reverse<i32>>) {
+    let s1 = lhs.len() as i32;
+    let s2 = rhs.len() as i32;
+    if s1 - s2 >= 1 {
+        rhs.push(Reverse(lhs.pop().unwrap()));
+    }
+    if s2 - s1 > 1 {
+        lhs.push(rhs.pop().unwrap().0);
+    }
+}
+// peek the top
+fn lhs_top(lhs: &BinaryHeap<i32>) -> i32 {
+    *lhs.peek().unwrap()
+}
+fn rhs_top(rhs: &BinaryHeap<Reverse<i32>>) -> i32 {
+    rhs.peek().unwrap().0
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 pub mod a1;
 pub mod a13;
 pub mod a1805;
+pub mod a480;
 pub mod a7;
 pub mod a9;
-pub mod a480;
 
 fn main() {
     println!("compile success!");
