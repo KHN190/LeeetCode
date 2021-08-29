53dda2741f5b1a9612c9e463fe88e9f4a426c0fc
knh190 <knh190@hotmail.com>
2021-08-29 03:43:44 -0400

    update.

---
+++ src/a133.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/clone-graph/
+
+// Given a reference of a node in a connected undirected graph.
+// Return a deep copy (clone) of the graph.
+
+// adjacency list: [[2,4],[1,3],[2,4],[1,3]]
+// or: [[]], single node graph
+
+// The number of nodes in the graph is in the range [0, 100].
+// Node.val is unique for each node. and equals its index.
+// There are no repeated edges and no self-loops in the graph.
+// The Graph is connected and all nodes can be visited starting from the given node.
---
+++ src/a187.rs
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/repeated-dna-sequences/
+
+// Given a string s that represents a DNA sequence,
+// return all the 10-letter-long sequences (substrings)
+// that occur more than once in a DNA molecule.
+// You may return the answer in any order.
+
+// 1 <= s.length <= 10^5
+
+pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+    use std::collections::HashSet;
+
+    if s.len() < 10 {
+        return vec![];
+    }
+
+    let mut found = HashSet::new();
+    let mut repeat = HashSet::new();
+
+    for i in 0..s.len() - 9 {
+        let cur = &s[i..i + 10];
+
+        if found.contains(cur) {
+            repeat.insert(cur.to_string());
+        } else {
+            found.insert(cur.to_string());
+        }
+    }
+    repeat.into_iter().collect()
+}
+
+#[test]
+fn run() {
+    let ans = find_repeated_dna_sequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".into());
+    assert_eq!(ans.len(), 2);
+
+    let ans = find_repeated_dna_sequences("AAAAAAAAAAAAA".into());
+    assert_eq!(ans.len(), 1);
+}
