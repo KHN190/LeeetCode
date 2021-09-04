488e0527c5c82ff2b6de6534485cd81aeaeb7af9
knh190 <knh190@hotmail.com>
2021-09-04 18:19:06 -0400

    update.

--- src/a239.rs
+++ src/a239.rs
@@ -4,9 +4,9 @@
 // size k which is moving from the very left of the array to the very right.
 // You can only see the k numbers in the window.
 // Each time the sliding window moves right by one position.
-
+//
 // Return the max sliding window.
-
+//
 // 1 <= nums.length <= 10^5
 // -10^4 <= nums[i] <= 10^4
 // 1 <= k <= nums.length
