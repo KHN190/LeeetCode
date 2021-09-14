8b6b7d32f02548a5d734fbb6a5e78e1719cfeda7
knh190 <knh190@hotmail.com>
2021-09-14 02:06:08 -0400

    #15. three sum. fmt

--- src/a15.rs
+++ src/a15.rs
@@ -10,17 +10,23 @@
 //  use index for access as much as possible.
 
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    if nums.len() < 3 { return vec![]; }
+    if nums.len() < 3 {
+        return vec![];
+    }
 
-    let mut ns:Vec<i32> = nums.clone();
+    let mut ns: Vec<i32> = nums.clone();
     let mut res = vec![];
     ns.sort();
 
-    for i in 0..nums.len()-2 {
+    for i in 0..nums.len() - 2 {
         // skip duplicate left value
-        if i > 0 && ns[i] == ns[i - 1] { continue; }
+        if i > 0 && ns[i] == ns[i - 1] {
+            continue;
+        }
         // break if sum must > 0
-        if ns[i] > 0 { break; }
+        if ns[i] > 0 {
+            break;
+        }
 
         let mut j = i + 1;
         let mut k = nums.len() - 1;
@@ -31,20 +37,19 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
             // iterate all triplets
             let sum = ns[i] + ns[j] + ns[k];
             if sum == 0 {
-                res.push(vec!(ns[i], ns[j], ns[k]));
+                res.push(vec![ns[i], ns[j], ns[k]]);
                 // no break, because we may have multiple
                 // results with same left value;
                 // and skip duplicates
-                while j < k && ns[j] == ns[j+1] {
+                while j < k && ns[j] == ns[j + 1] {
                     j += 1;
                 }
-                while j < k && ns[k] == ns[k-1] {
+                while j < k && ns[k] == ns[k - 1] {
                     k -= 1;
                 }
                 j += 1;
                 k -= 1;
-            }
-            else if sum < 0 {
+            } else if sum < 0 {
                 j += 1;
             } else {
                 k -= 1;
@@ -56,7 +61,7 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
 
 #[test]
 fn run() {
-    let nums = vec![3,0,-2,-1,1,2];
+    let nums = vec![3, 0, -2, -1, 1, 2];
     let mut res = three_sum(nums);
     res.sort();
 
@@ -66,7 +71,7 @@ fn run() {
     assert_eq!(res[2], vec![-1, 0, 1]);
 
     // no duplicates
-    let nums = vec![0,0,0,0];
+    let nums = vec![0, 0, 0, 0];
     let res = three_sum(nums);
 
     assert_eq!(res.len(), 1);
