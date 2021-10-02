7b4b98155a24571cfd5bc0933c613255c13ad294
knh190 <knh190@hotmail.com>
2021-10-01 20:40:57 -0400

    #98. Use shared data structure.

--- src/a98.rs
+++ src/a98.rs
@@ -12,23 +12,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
+use crate::types::TreeNode;
 
 pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     valid(root, std::i64::MIN, std::i64::MAX)
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,5 @@
 mod list_node;
+mod tree_node;
 
 pub use list_node::ListNode;
+pub use tree_node::TreeNode;
