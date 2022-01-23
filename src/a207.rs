4848c1d34efeed2490223fead9bea355e716d9c8
knh190 <knh190@hotmail.com>
2022-01-23 17:37:22 -0500

    #207. Detect cycle in graph.

--- src/a207.rs
+++ src/a207.rs
@@ -2,32 +2,56 @@
 
 // detect cycle in a graph. if there's a cycle,
 // then impossible to finish all course.
-// cycle means starts from a point, you can visit it again.
 
-use std::collections::HashMap;
-use std::collections::HashSet;
+const PREREQUISITE: usize = 0;
+const COURSE: usize = 1;
 
-// 0: being visited, 1: visited, -1: not visited
-pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
-    let (graph, nodes) = create_graph(prerequisites);
-    let visited = vec![-1; nodes.len()];
-
-    for node in nodes {
-        //
-    }
-
-    false
+#[derive(Copy, Clone)]
+enum Status {
+    NotVisited,
+    Visiting,
+    Visited,
 }
 
-// return graph and all nodes
-fn create_graph(edges: Vec<Vec<i32>>) -> (HashMap<i32, Vec<i32>>, Vec<i32>) {
-    let mut graph = HashMap::<i32, Vec<i32>>::new();
-    let mut nodes = HashSet::<i32>::new();
-    for edge in edges {
-        let (k, v) = (edge[1], edge[0]);
-        graph.entry(k).or_insert(vec![]).push(v);
-        nodes.insert(k);
-        nodes.insert(v);
+pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+    let num_courses = num_courses as usize;
+
+    // dependencies between courses
+    let mut graph = vec![Vec::new(); num_courses];
+    for edge in prerequisites.iter() {
+        // hashtable itself is a vector of vector
+        graph[edge[PREREQUISITE] as usize].push(edge[COURSE] as usize);
     }
-    (graph, nodes.into_iter().collect())
+
+    let mut status = vec![Status::NotVisited; num_courses];
+    (0..num_courses).all(|course| !has_cycle(course, &mut status, &graph))
+}
+
+fn has_cycle(course: usize, status: &mut Vec<Status>, graph: &Vec<Vec<usize>>) -> bool {
+    match status[course] {
+        Status::Visited => false,
+        Status::Visiting => true,
+        _ => {
+            status[course] = Status::Visiting;
+            if graph[course]
+                .iter()
+                .any(|&next_course| has_cycle(next_course, status, graph))
+            {
+                return true;
+            }
+            status[course] = Status::Visited;
+            false
+        }
+    }
+}
+
+#[test]
+fn run() {
+    let n = 2;
+    let pre: Vec<Vec<i32>> = vec![vec![1, 0]];
+    assert_eq!(can_finish(n, pre), true);
+
+    let n = 2;
+    let pre: Vec<Vec<i32>> = vec![vec![1, 0], vec![0, 1]];
+    assert_eq!(can_finish(n, pre), false);
 }
