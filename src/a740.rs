4eca35d28bcb295434d17ff0e9af94ad68b90eb9
knh190 <knh190@hotmail.com>
2022-01-23 17:24:24 -0500

    #740. improve mem usage.

--- src/a740.rs
+++ src/a740.rs
@@ -3,22 +3,19 @@
 // del nums[i] to earn score
 // after it, del all fs equal to nums[i] - 1 or nums[i] + 1
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 pub fn delete_and_earn(nums: Vec<i32>) -> i32 {
     // occurrences of each number, k is the n
     let mut fs = HashMap::<i32, i32>::new();
-    let mut ks = HashSet::<i32>::new();
+    // min and max of all nums
+    let (mut s1, mut s2) = (0, 0);
     for n in nums {
         let cnt = fs.entry(n).or_insert(0);
         *cnt += 1;
-        ks.insert(n);
+        s1 = s1.min(n);
+        s2 = s2.max(n);
     }
-    // min and max of all nums
-    let (s1, s2) = (
-        *ks.iter().min().unwrap() as usize,
-        *ks.iter().max().unwrap() as usize,
-    );
     // then question becomes:
     // we can earn from n in [s1..s2], but cannot rob n-1 or n+1
     let mut res: Vec<i32> = vec![];
@@ -28,9 +25,13 @@ pub fn delete_and_earn(nums: Vec<i32>) -> i32 {
 
         if i >= 2 {
             res.push((res[i - 2] + cur).max(res[i - 1]));
-        } else if i >= 1 {
+            continue;
+        }
+        if i == 1 {
             res.push(cur.max(res[i - 1]));
-        } else {
+            continue;
+        }
+        if i == 0 {
             res.push(cur);
         }
     }
