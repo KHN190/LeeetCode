626d1f3c2de6ee7e7fef1c392bdd0362d69f38eb
knh190 <knh190@hotmail.com>
2022-02-01 17:26:57 -0500

    #684. Find redundant edge.

--- src/a684.rs
+++ src/a684.rs
@@ -3,16 +3,51 @@
 // return the last edge can be removed
 // detect cycle
 
-use std::collections::HashSet;
+// 1. DFS
+// 2. Union Find
+// 3. Disjoint Set
 
-pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
-    vec![]
+#[derive(Default)]
+pub struct UnionFind {
+    parent: Vec<usize>,
 }
 
-fn dfs(curr: i32, edges: &Vec<Vec<i32>>, visited: &mut HashSet<i32>, dups: &mut Vec<i32>) {
-    // if edge ends with visited, it can be removed
-    if visited.contains(&curr) {
-        return;
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+        }
     }
-    visited.insert(curr);
+    fn union(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+        self.parent[y] = x
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if x != self.parent[x] {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+}
+
+pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut uf = UnionFind::new(edges.len());
+    for edge in edges {
+        let (e0, e1) = (edge[0] as usize - 1, edge[1] as usize - 1);
+        if uf.find(e0) == uf.find(e1) {
+            return edge;
+        }
+        uf.union(e0, e1);
+    }
+    unreachable!()
+}
+
+#[test]
+fn run() {
+    let edges = vec![vec![1, 2], vec![1, 3], vec![2, 3]];
+    assert_eq!(find_redundant_connection(edges), vec![2, 3]);
+
+    let edges = vec![vec![1, 2], vec![2, 3], vec![3, 4], vec![1, 4], vec![1, 5]];
+    assert_eq!(find_redundant_connection(edges), vec![1, 4]);
 }
