6e2b07157217cc685cbdbd29c75dbec13163af44
knh190 <knh190@hotmail.com>
2022-02-01 15:36:42 -0500

    #50. Power of num (notice negative power).

--- src/a268_s2.rs
+++ src/a268_s2.rs
@@ -4,7 +4,9 @@ pub fn missing_number(nums: Vec<i32>) -> bool {
     let mut n: i32 = 0;
     let mut sum: i32 = 0;
     for i in nums.iter() {
-        if i > &n { n = *i; }
+        if i > &n {
+            n = *i;
+        }
         sum += i;
     }
     (n + 1) * n / 2 != sum
---
+++ src/a50.rs
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/powx-n/
+
+pub fn my_pow(x: f64, n: i32) -> f64 {
+    let mut res = 1f64;
+    let (mut n, mut x) = (n, x);
+    if n < 0 {
+        n *= -1;
+        x = 1.0 / x;
+    }
+    for _ in 0..n as usize {
+        res *= x;
+    }
+    res
+}
+
+#[test]
+fn run() {
+    assert_eq!(my_pow(2.0, 10), 1024.0);
+    assert_eq!(my_pow(2.0, -2), 0.25);
+}
