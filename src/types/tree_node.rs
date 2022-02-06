a8cd513b2eecf4b2987abe23fb289c51c8e06afe
knh190 <knh190@hotmail.com>
2022-02-06 16:38:37 -0500

    Add tree visit methods.

--- src/types/tree_node.rs
+++ src/types/tree_node.rs
@@ -8,6 +8,12 @@ pub struct TreeNode {
     pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
+pub enum TraverseOrder {
+    Inorder,
+    Preorder,
+    Postorder,
+}
+
 impl TreeNode {
     #[inline]
     pub fn new(val: i32) -> Self {
@@ -18,13 +24,11 @@ impl TreeNode {
         }
     }
 
-    pub fn from_vec(v: &Vec<i32>) -> Option<Self> {
-        if v.len() == 0 {
-            return None;
-        }
+    pub fn from_vec(v: &Vec<i32>) -> Self {
+        assert!(v.len() > 0);
         let mut root = TreeNode::new(v[0]);
-        root._insert(v, 0);
-        Some(root)
+        root.inorder_insert(v, 0);
+        root
     }
 
     pub fn set_lhs(&mut self, val: i32) -> &mut Option<Rc<RefCell<Self>>> {
@@ -37,40 +41,75 @@ impl TreeNode {
         &mut self.right
     }
 
-    // inorder visit
     pub fn visit(&self) -> Vec<i32> {
+        self.visit_with(None)
+    }
+
+    pub fn visit_with(&self, order: Option<TraverseOrder>) -> Vec<i32> {
         let mut res: Vec<i32> = vec![];
-        self._visit(&mut res);
+        match order {
+            Some(TraverseOrder::Preorder) => self.preorder(&mut res),
+            Some(TraverseOrder::Inorder) => self.inorder(&mut res),
+            Some(TraverseOrder::Postorder) => self.postorder(&mut res),
+            _ => self.preorder(&mut res),
+        }
         return res;
     }
 
-    fn _insert(self: &mut Self, v: &Vec<i32>, i: usize) -> &mut Self {
+    fn inorder_insert(self: &mut Self, v: &Vec<i32>, i: usize) -> &mut Self {
         if 2 * i + 1 < v.len() && self.left.is_none() {
             // println!("insert lhs: {} to {}", v[2 * i + 1], 2 * i + 1);
             self.set_lhs(v[2 * i + 1]);
             let lhs: &mut TreeNode = &mut self.left.as_ref().unwrap().borrow_mut();
-            lhs._insert(v, 2 * i + 1);
+            lhs.inorder_insert(v, 2 * i + 1);
         }
+        self.val = v[i];
+
         if 2 * i + 2 < v.len() && self.right.is_none() {
             // println!("insert rhs: {} to {}", v[2 * i + 2], 2 * i + 2);
             self.set_rhs(v[2 * i + 2]);
             let rhs: &mut TreeNode = &mut self.right.as_ref().unwrap().borrow_mut();
-            rhs._insert(v, 2 * i + 2);
+            rhs.inorder_insert(v, 2 * i + 2);
         }
         self
     }
 
-    fn _visit(&self, res: &mut Vec<i32>) {
+    fn preorder(&self, res: &mut Vec<i32>) {
         res.push(self.val);
 
         if self.left.is_some() {
             let lhs: &TreeNode = &self.left.as_ref().unwrap().borrow();
-            lhs._visit(res);
+            lhs.preorder(res);
         }
         if self.right.is_some() {
             let rhs: &TreeNode = &self.right.as_ref().unwrap().borrow();
-            rhs._visit(res);
+            rhs.preorder(res);
         }
+    }
+
+    fn inorder(&self, res: &mut Vec<i32>) {
+        if self.left.is_some() {
+            let lhs: &TreeNode = &self.left.as_ref().unwrap().borrow();
+            lhs.inorder(res);
+        }
+        res.push(self.val);
+
+        if self.right.is_some() {
+            let rhs: &TreeNode = &self.right.as_ref().unwrap().borrow();
+            rhs.inorder(res);
+        }
+    }
+
+    fn postorder(&self, res: &mut Vec<i32>) {
+        if self.left.is_some() {
+            let lhs: &TreeNode = &self.left.as_ref().unwrap().borrow();
+            lhs.postorder(res);
+        }
+        if self.right.is_some() {
+            let rhs: &TreeNode = &self.right.as_ref().unwrap().borrow();
+            rhs.postorder(res);
+        }
+        res.push(self.val);
     }
 }
 
@@ -78,7 +117,7 @@ impl TreeNode {
 fn run() {
     let v = vec![1, 2, 3, 4, 5];
     let t = TreeNode::from_vec(&v);
-    let ns = t.unwrap().visit();
+    let ns = t.visit();
 
     assert_eq!(ns, vec![1, 2, 4, 5, 3]);
 }
