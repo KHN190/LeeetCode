93f035e2db67391a6568fda2321e913e772c7199
knh190 <knh190@hotmail.com>
2022-02-14 17:27:06 -0500

    #219. Find dup in window.

---
+++ src/a219.rs
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/contains-duplicate-ii/
+
+// return true if nums[i] == nums[j] and abs(i - j) <= k. i != j
+
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+    let size = nums.len();
+    if size <= 1 {
+        return false;
+    }
+    // keep a set of curr n elements, n <= k
+    let k = k as usize;
+    let mut window = VecDeque::<i32>::with_capacity(k);
+    let mut elems = HashSet::<i32>::new();
+    for i in 0..size {
+        // find if next elem already in the window
+        if elems.contains(&nums[i]) {
+            return true;
+        }
+        // if not, slide the window
+        window.push_back(nums[i]);
+        elems.insert(nums[i]);
+        if window.len() > k {
+            elems.remove(&window.pop_front().unwrap());
+        }
+    }
+    false
+}
+
+#[test]
+fn run() {
+    let nums = vec![1, 2, 3, 1];
+    assert!(contains_nearby_duplicate(nums, 3));
+
+    let nums = vec![1, 2, 3, 1];
+    assert!(!contains_nearby_duplicate(nums, 2));
+}
---
+++ src/a220.rs
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/contains-duplicate-iii/
+
+// true if abs(nums[i] - nums[j]) <= t, abs(i - j) <= k. i != j.
+
+pub fn contains_nearby_almost_duplicate(nums: Vec<i32>, k: i32, t: i32) -> bool {
+    false
+}
