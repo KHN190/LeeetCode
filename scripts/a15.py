625aaa5af0a74d9b938306d567da837cec05faf3
knh190 <knh190@hotmail.com>
2022-04-14 14:44:43 -0400

    Arrange str, char by freq.

---
+++ scripts/a15.py
@@ -0,0 +1,9 @@
+def rearangeStr(s):
+    ns = collections.Counter(s)
+    tmp = sorted([x for x in ns.items()], key=lambda x: x[1], reverse=True)
+    res = ''
+    for x in tmp:
+        res += x[0] * x[1]
+    return res
+
+assert rearangeStr('abbabc') == 'bbbaac'
